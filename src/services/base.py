cc817f36fde1add6029999fa5eb64e26c717f2e8
Ivan Bazhenov <bazhenov.in@gmail.com>
2023-05-16 22:54:59 +0300

    [#41] Добавление базовых сервисов

---
+++ src/services/base.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+from . import AbstractService
+from .elastic_db import ElasticRequest
+from .redis_cache import RedisCache
+
+
+class BaseService(AbstractService, ABC):
+    def __init__(self, cache: RedisCache, request: ElasticRequest):
+        self.cache = cache
+        self.request = request
+
+    async def get_by_id(self, id_: str):
+        """Метод получения объекта по id.
+        """
+        key = f'id:{id_}'
+        data = await self.cache.get_from_cache(key)
+
+        if data is not None:
+            entity = self.request.model.parse_raw(data)
+
+        else:
+            entity = await self.request.get_by_id(id_)
+            if not entity:
+                return None
+
+            data = entity.json(by_alias=True)
+            await self.cache.put_to_cache(key, data)
+
+        return entity
+
+    @abstractmethod
+    async def filter(self, **kwargs):
+        """Метод получения списка объектов по параметрам.
+        """
+        pass
